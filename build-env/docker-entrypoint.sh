213418be378ec1dd11f4920ab1ede6c3cf1bcdbf
SteBaum <stephan.baum@edu.dsti.institute>
2025-09-01 11:37:04 +0200

    refactor(tdp-builder): revert to previous docker-entrypoint.sh

--- build-env/docker-entrypoint.sh
+++ build-env/docker-entrypoint.sh
@@ -3,9 +3,9 @@ set -eo pipefail
 
 if [[ -n "$BUILDER_UID" ]] && [[ -n "$BUILDER_GID" ]]; then
     # Create group and user only if they don't exist
-    [[ ! $(getent group builder) ]] && groupadd --gid "$BUILDER_GID" --system builder
-    if [[ ! $(getent passwd builder) ]]; then
-        useradd --uid "$BUILDER_UID" --system --gid builder --home-dir /home/builder builder
+    [[ ! $(getent group "$BUILDER_GID") ]] && groupadd --gid "$BUILDER_GID" --system builder
+    if [[ ! $(getent passwd "$BUILDER_UID") ]]; then
+        useradd --uid "$BUILDER_UID" --system --gid "$BUILDER_GID" --home-dir /home/builder builder
         # Avoid useradd warning if home dir already exists by making home dir ourselves.
         # Home dir can exists if mounted via "docker run -v ...:/home/builder/...".
         mkdir -p /home/builder
@@ -15,9 +15,9 @@ if [[ -n "$BUILDER_UID" ]] && [[ -n "$BUILDER_GID" ]]; then
     # Avoid changing dir if a work dir is specified
     [[ "$PWD" == "/root" ]] && cd /home/builder
     if [[ -z "$@" ]]; then
-        exec gosu builder /bin/bash
+        exec gosu $BUILDER_UID /bin/bash
     else
-        exec gosu builder "$@"
+        exec gosu $BUILDER_UID "$@"
     fi
 fi
 
